a755c9ee42190e94127afa3175615afbc78f23e7
DMP9 <dylanperks23@gmail.com>
2018-10-07 21:21:02 +0100

    Less duplicate code

--- Examples/ExampleServer/Program.cs
+++ Examples/ExampleServer/Program.cs
@@ -22,51 +22,12 @@ class Program
 
     static bool AcceptIncomingStream(IStream stream)
     {
-        Task.Run(() => HandleIncomingStream(stream));
+        Task.Run(() => ExampleShared.HandleIncomingStream(stream,responseBody));
         return true;
     }
 
     static byte[] responseBody = Encoding.ASCII.GetBytes(
         "<html><head>Hello World</head><body>Content</body></html>");
-
-    static async void HandleIncomingStream(IStream stream)
-    {
-        try
-        {
-            // Read the headers
-            var headers = await stream.ReadHeadersAsync();
-            var method = headers.First(h => h.Name == ":method").Value;
-            var path = headers.First(h => h.Name == ":path").Value;
-            // Print the request method and path
-            Console.WriteLine("Method: {0}, Path: {1}", method, path);
-
-            // Read the request body and write it to console
-            var buf = new byte[2048];
-            while (true)
-            {
-                var readResult = await stream.ReadAsync(new ArraySegment<byte>(buf));
-                if (readResult.EndOfStream) break;
-                // Print the received bytes
-                Console.WriteLine(Encoding.ASCII.GetString(buf, 0, readResult.BytesRead));
-            }
-
-            // Send a response which consists of headers and a payload
-            var responseHeaders = new HeaderField[] {
-                new HeaderField { Name = ":status", Value = "200" },
-                new HeaderField { Name = "content-type", Value = "text/html" },
-            };
-            await stream.WriteHeadersAsync(responseHeaders, false);
-            await stream.WriteAsync(new ArraySegment<byte>(
-                responseBody), true);
-
-            // Request is fully handled here
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error during handling request: {0}", e.Message);
-            stream.Cancel();
-        }
-    }
 
     static async Task AcceptTask(TcpListener listener, ILoggerProvider logProvider)
     {
--- Examples/HttpsExampleServer/Program.cs
+++ Examples/HttpsExampleServer/Program.cs
@@ -29,7 +29,7 @@ class Program
 
     static bool AcceptIncomingStream(IStream stream)
     {
-        Task.Run(() => HandleIncomingStream(stream));
+        Task.Run(() => ExampleShared.HandleIncomingStream(stream,responseBody));
         return true;
     }
 
@@ -60,46 +60,6 @@ class Program
             }
 
             return ms.ToArray();
-        }
-    }
-
-    static async void HandleIncomingStream(IStream stream)
-    {
-        try
-        {
-            // Read the headers
-            var headers = await stream.ReadHeadersAsync();
-            var method = headers.First(h => h.Name == ":method").Value;
-            var path = headers.First(h => h.Name == ":path").Value;
-            // Print the request method and path
-            Console.WriteLine("Method: {0}, Path: {1}", method, path);
-
-            // Read the request body and write it to console
-            var buf = new byte[2048];
-            while (true)
-            {
-                var readResult = await stream.ReadAsync(new ArraySegment<byte>(buf));
-                if (readResult.EndOfStream) break;
-                // Print the received bytes
-                Console.WriteLine(Encoding.ASCII.GetString(buf, 0, readResult.BytesRead));
-            }
-
-            // Send a response which consists of headers and a payload
-            var responseHeaders = new HeaderField[]
-            {
-                new HeaderField {Name = ":status", Value = "200"},
-                new HeaderField {Name = "content-type", Value = "text/html"},
-            };
-            await stream.WriteHeadersAsync(responseHeaders, false);
-            await stream.WriteAsync(new ArraySegment<byte>(
-                responseBody), true);
-
-            // Request is fully handled here
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error during handling request: {0}", e.Message);
-            stream.Cancel();
         }
     }
 
